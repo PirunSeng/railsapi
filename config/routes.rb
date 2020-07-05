efb212f55104b770b4194b71a0c8e77cad75e0c1
PirunSeng <sengpirun.rain@gmail.com>
2020-07-05 14:37:41 +0700

    enables index routes only for now

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/' do
 
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :products
+      resources :products, only: [:index]
     end
   end
 end
